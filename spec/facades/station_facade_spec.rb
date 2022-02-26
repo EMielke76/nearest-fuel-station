d6be6bd4d8429356b8aaba895e5faa36f01c1051
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-02-26 13:59:10 -0700

    Feat: StationFacade passing tests

--- spec/facades/station_facade_spec.rb
+++ spec/facades/station_facade_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 RSpec.describe StationFacade do
   describe 'instance methods' do
-    describe '.station_search(address)' do
+    describe '.find_search(address)' do
       it "returns Station data", :vcr do
         query = StationFacade.new
-        results = query.station_search('1331 17th St, Denver, CO 80202')
-
+        results = query.find_station('1331 17th St, Denver, CO 80202')
+        
         expect(results).to be_a(Array)
         results.each do |result|
           expect(result).to be_a(Station)
-        end 
+        end
       end
     end
   end
