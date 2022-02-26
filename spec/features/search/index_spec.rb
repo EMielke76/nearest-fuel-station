aa6e22bd0aa3bdd3dacfe1943836cf1baf59f0f6
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-02-26 13:10:00 -0700

    Create test suite for search result index

---
+++ spec/features/search/index_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'Search Results Index' do
+  # Then I should see the closest electric fuel station to me.
+  #
+  # For that station I should see
+  # - Name
+  # - Address
+  # - Fuel Type
+  # - Access Times
+  #
+  # I should also see:
+  # - the distance of the nearest station (should be 0.1 miles)
+  # - the travel time from Turing to that fuel station (should be 1 min)
+  # - The direction instructions to get to that fuel station
+  #   "Turn left onto Lawrence St Destination will be on the left"
+  it 'displays the neartest station and its attributes' do
+  end
+
+  it 'displays the distance, travel time, and directions to the the nearest station' do 
+  end
+end
