8146f6dea147eda9eeeb810ef57f933c66109a41
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-02-26 15:21:28 -0700

    Feat: Search Index displaying directions information

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,5 +2,6 @@ class SearchController < ApplicationController
 
   def index
     @station = StationFacade.new.find_station(params[:location]).first
+    @directions = DirectionsFacade.new.get_directions(params[:location], @station.address)
   end
 end
